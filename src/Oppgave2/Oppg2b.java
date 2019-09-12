ff21b785b2c20fcc54b76ba41781e10399ee4cfe
hvl578005 <kath.hermansen@gmail.com>
2019-09-12 15:33:06 +0200

    Oppdatering

--- src/Oppgave2/Oppg2b.java
+++ src/Oppgave2/Oppg2b.java
@@ -4,36 +4,21 @@ import java.util.function.BiFunction;
 
 public class Oppg2b {
 	
-	public static int beregn(int a, int b, String beregning) {
+	public static int beregn(int a, int b, BiFunction<Integer, Integer, Integer> beregn) {
+		
+		return beregn.apply(a, b);
+		
+	}
+
+	public static void main(String[] args) {
 		
 		BiFunction<Integer, Integer, Integer> summer = (s1, s2) -> s1+s2;
 		BiFunction<Integer, Integer, Integer> stoerreEnn = (s1, s2) -> Math.max(s1, s2);
 		BiFunction<Integer, Integer, Integer> differanse = (s1, s2) -> Math.abs(s1-s2);
 		
-		int svar = 0;
-		
-		if (beregning == "summer") {
-			svar = summer.apply(a, b);
-			System.out.println(svar);
-		} else if (beregning == "stoerreEnn") {
-			svar = stoerreEnn.apply(a, b);
-			System.out.println(svar);
-		} else if (beregning == "differanse") {
-			svar = differanse.apply(a, b);
-			System.out.println(svar);
-		} else {
-			System.out.println("Ikke gyldig beregning.");
-		}
-  		
-		return svar;	
-	}
-
-	public static void main(String[] args) {
-		
-		beregn(12, 13, "summer");
-		beregn(-5, 3, "stoerreEnn");
-		beregn(54, 45, "differanse");
-		beregn (1, 2, "test");
+		beregn(12, 13, summer);
+		beregn(-5, 3, stoerreEnn);
+		beregn(54, 45, differanse);
 
 	}
 	
--- src/Oppgave3/Oppg3.java
+++ src/Oppgave3/Oppg3.java
@@ -3,10 +3,16 @@ package Oppgave3;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Oppg3 {
 	
-	private static void lonnsoppgjor(List<Ansatt> ansatte, ???) {
+	private static void lonnsoppgjor(List<Ansatt> ansatte, Function<Ansatt, Integer> fun) {
+		
+		for (int i = 0; i<ansatte.size(); i++) {
+			ansatte.get(i).setAarslonn(fun.apply(ansatte.get(i)));
+		}
 		
 	}
 
@@ -18,11 +24,43 @@ public class Oppg3 {
 				new Ansatt("Vilde Kristine", "Fossum", Kjonn.Kvinne, "IT-konsulent", 900000),
 				new Ansatt("Francis", "Sollimann", Kjonn.Mann, "Sekretær", 150000),
 				new Ansatt("Lasse", "Hansen", Kjonn.Mann, "Elektriker", 550000),
-				new Ansatt("Bjørnar", "Espeland", Kjonn.Mann, "Svømmer", 520000),
-				new Ansatt("Malin", "Iversen", Kjonn.Kvinne, "Sminkør", 700000)
+				new Ansatt("Bjørnar", "Espeland", Kjonn.Mann, "Svømmer", 5000),
+				new Ansatt("Malin", "Iversen", Kjonn.Kvinne, "Sminkør", 800000),
+				new Ansatt("Fredrik", "Mørk", Kjonn.Mann, "Kjæreste", 950000)
 				);
-			
-
+		
+		//Et fast kronetillegg med 10 000
+		Function<Ansatt, Integer> kroneTillegg = (ansatt) -> ansatt.getAarslonn() + 10000;
+		
+		//Et fast prosenttillegg
+		Function<Ansatt, Integer> prosentTillegg = (ansatt) -> (ansatt.getAarslonn()*5)/100 + ansatt.getAarslonn();
+		
+		//Et fast kronetillegg hvis du har lav lønn
+		Function<Ansatt, Integer> lavLønnKr = (ansatt) -> { 
+			if (ansatt.getAarslonn() <= 600000) {
+				return (ansatt.getAarslonn()*15)/100 + ansatt.getAarslonn();
+			} else {
+				return ansatt.getAarslonn();
+			}
+		};
+		
+		//Et fast prosenttilegg hvis du er mann
+		Function<Ansatt, Integer> prosentMann = (ansatt) -> {
+			if (ansatt.getKjonn() == Kjonn.Mann) {
+				return (ansatt.getAarslonn()*2)/100 + ansatt.getAarslonn();
+			} else {
+				return ansatt.getAarslonn();
+			}
+		};
+		
+		lonnsoppgjor(ansatte, kroneTillegg);
+		lonnsoppgjor(ansatte, prosentTillegg);
+		lonnsoppgjor(ansatte, lavLønnKr);
+		lonnsoppgjor(ansatte, prosentMann);
+		ansatte.forEach(System.out::println);
+		
 	}
 
 }
+
+
